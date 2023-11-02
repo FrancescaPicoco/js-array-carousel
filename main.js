78f7c4adf2937a080d3396b54de4f24eef4e9557
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-02 21:37:15 +0100

    tryin bonus

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ let picsPath = ["img/01.webp" , "img/02.webp" , "img/03.webp" , "img/04.webp" , 
 for (let i = 0; i < picsPath.length; i++) {
     const picPath = picsPath[i];
     console.log(picPath);
-    document.getElementById("wrapper").innerHTML += `<img src="${picPath}">`;  
+    document.getElementById("picsHere").innerHTML += `<img src="${picPath}">`;  
 }
 //document.getElementById("wrapper").innerHTML = picsHtml;
  
 let current = 0;
 
-const pics = document.querySelectorAll(".wrapper img");
+const pics = document.querySelectorAll("#picsHere img");
 pics[current].classList.add("visible");
 
 document.getElementById("next").addEventListener("click" , function(params) {
@@ -39,6 +39,6 @@ document.getElementById("previous").addEventListener("click" , function(params) 
     }
 })
 document.querySelectorAll("roundBtn").addEventListener("click" , function(params) {
-    
+
     
 })
