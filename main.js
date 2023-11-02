dc678986dff4cc1364b797baed4065a9bd0a1d2c
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-02 19:50:34 +0100

    js img

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+
+let picsPath = ["img/01.webp" , "img/02.webp" , "img/03.webp" , "img/04.webp" , "img/05.webp"];
+
+//let picsHtml = ""
+for (let i = 0; i < picsPath.length; i++) {
+    const picPath = picsPath[i];
+    console.log(picPath);
+    document.getElementById("wrapper").innerHTML += `<img src="${picPath}">`;  
+}
+//document.getElementById("wrapper").innerHTML = picsHtml;
+ 
 let current = 0;
 
 const pics = document.querySelectorAll(".wrapper img");
@@ -9,13 +20,21 @@ document.getElementById("next").addEventListener("click" , function(params) {
         current++;
         console.log(current)
         pics[current].classList.add("visible");
-    }
+    }    
 })
+/*for (let i = 0; i < picsPath.length; i++) {
+    const continuous = picsPath[i];
+    console.log(continuous);
+    if (current == 4){
+        continue;
+    }
+}*/
 
 document.getElementById("previous").addEventListener("click" , function(params) {
     if (current > 0 ) {
         pics[current].classList.remove("visible");
         current--;
+        console.log(current)
         pics[current].classList.add("visible");
     }
 })
