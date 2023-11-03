c4939f8f3bb43639f90e0871b9b433985e77739b
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-03 15:13:23 +0100

    bonus 2 prova

--- main.js
+++ main.js
@@ -5,21 +5,27 @@ let picsPath = ["img/01.webp" , "img/02.webp" , "img/03.webp" , "img/04.webp" , 
 for (let i = 0; i < picsPath.length; i++) {
     const picPath = picsPath[i];
     console.log(picPath);
-    document.getElementById("picsHere").innerHTML += `<img src="${picPath}">`;  
+    document.getElementById("picsHere").innerHTML += `<img src="${picPath}">`;
+    document.getElementById("right").innerHTML += `<img src="${picPath}">`;
+     
 }
 //document.getElementById("wrapper").innerHTML = picsHtml;
  
 let current = 0;
-
+let currentSide = 0
 const pics = document.querySelectorAll("#picsHere img");
 pics[current].classList.add("visible");
+const sidePics = document.querySelectorAll("#right img");
+sidePics[currentSide].classList.add("opacity");
 
 document.getElementById("next").addEventListener("click" , function(params) {
     if (current < pics.length-1) {
         pics[current].classList.remove("visible");
+        sidePics[currentSide].classList.remove("opacity");
         current++;
         console.log(current)
         pics[current].classList.add("visible");
+        sidePics[currentSide].classList.add("opacity");
     }    
 })
 /*for (let i = 0; i < picsPath.length; i++) {
@@ -38,6 +44,7 @@ document.getElementById("previous").addEventListener("click" , function(params) 
         pics[current].classList.add("visible");
     }
 })
+
 document.querySelectorAll("roundBtn").addEventListener("click" , function(params) {
 
     
