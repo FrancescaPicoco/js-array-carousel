ff9ad375185810efb1573b3e91ef4f84a859f264
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-03 18:30:20 +0100

    opacità strano comportamento

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 let picsPath = ["img/01.webp" , "img/02.webp" , "img/03.webp" , "img/04.webp" , "img/05.webp"];
-
+let sidePath =["img/01.webp" , "img/02.webp" , "img/03.webp" , "img/04.webp" , "img/05.webp"];
 //let picsHtml = ""
 for (let i = 0; i < picsPath.length; i++) {
     const picPath = picsPath[i];
@@ -12,41 +12,41 @@ for (let i = 0; i < picsPath.length; i++) {
 //document.getElementById("wrapper").innerHTML = picsHtml;
  
 let current = 0;
-let currentSide = 0
+//let currentSide = 0;
 const pics = document.querySelectorAll("#picsHere img");
 pics[current].classList.add("visible");
 const sidePics = document.querySelectorAll("#right img");
-sidePics[currentSide].classList.add("opacity");
+sidePics[current].classList.remove("opacity");
 
 document.getElementById("next").addEventListener("click" , function(params) {
     if (current < pics.length-1) {
         pics[current].classList.remove("visible");
-        sidePics[currentSide].classList.remove("opacity");
+        sidePics[current].classList.add("opacity");
         current++;
-        currentSide++;
+        //currentSide++;
         console.log(current)
         pics[current].classList.add("visible");
-        sidePics[currentSide].classList.add("opacity");
+        sidePics[current].classList.remove("opacity");
+        }else if (current == pics.length -1) {         
+        pics[current].classList.remove("visible");       
+        current = 0;                         
+        pics[current].classList.add("visible");      
     }    
 })
-/*for (let i = 0; i < picsPath.length; i++) {
-    const continuous = picsPath[i];
-    console.log(continuous);
-    if (current == 4){
-        continue;
-    }
-}*/
 
 document.getElementById("previous").addEventListener("click" , function(params) {
     if (current > 0 ) {
         pics[current].classList.remove("visible");
+        sidePics[current].classList.add("opacity");
         current--;
+        //currentSide--;
         console.log(current)
         pics[current].classList.add("visible");
-    }
-})
-
-document.querySelectorAll("roundBtn").addEventListener("click" , function(params) {
-
+        sidePics[current].classList.remove("opacity");
+    }else if (current == 0) {             
+        pics[current].classList.remove("visible");       
+        current = pics.length -1;              
+        pics[current].classList.add("visible"); 
+    }    
     
 })
